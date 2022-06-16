68dcc16d3a27ae8a3dd08530f029d670f5ae3eea
nebyou16 <104837818+nebyou16@users.noreply.github.com>
2022-06-16 20:35:05 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
-* rot13 -  a   function that encodes a string using rot13.
-* @s: An input string to encode using rot13
-* Return: An encode string
+* *rot13 - encodes a string using rot13.
+*
+* @str: string.
+* Return: string.
 */
-char *rot13(char *s)
+char *rot13(char *str)
 {
-int i = 0;
-while (s[i] != '\0')
+int i;
+for (i = 0; str[i] != '\0'; i++)
 {
-while ((s[i] >= 'a' && s[i] <= 'z') ||
-(s[i] >= 'A' && s[i] <= 'Z'))
+if ((*(str + i) >= 'a' && *(str + i) < 'n')
+|| (*(str + i) >= 'A' && *(str + i) < 'N'))
 {
-if ((s[i] >= 'a' && s[i] <= 'm') ||
-(s[i] >= 'A' && s[i] <= 'M'))
-s[i] += 13;
-else
-s[i] -= 13;
-i++;
+*(str + i) += 13;
 }
-i++;
+else if ((*(str + i) >= 'n' && *(str + i) <= 'z')
+|| (*(str + i) >= 'N' && *(str + i) <= 'Z'))
+{
+*(str + i) -= 13;
 }
-return (s);
+}
+return (str);
 }
