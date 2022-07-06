4e95444bb99e93d28d8e1526caac9cfdfeeac4cf
nebyou16 <104837818+nebyou16@users.noreply.github.com>
2022-07-06 17:56:41 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,45 +1,39 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
-* op_add - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
+* op_add - adds two numbers
+* @a: first number.
+* @b: second number.
+* Return: a + b.
 */
 int op_add(int a, int b)
 {
 return (a + b);
 }
 /**
-* op_sub - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
+* op_sub - difference of two numbers
+* @a: first number.
+* @b: second number.
+* Return: a - b.
 */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
 /**
-* op_mul - ...
-* @a: ...
-* @b: ...
-*
-*  Return: ...
+* op_mul - multiplies two numbers
+* @a: first number.
+* @b: second number.
+* Return: a * b.
 */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
 /**
-* op_div - ...
-* @a: ...
-* @b: ...
-*
-*  Return: ...
+* op_div - divides two numbers
+* @a: first number.
+* @b: second number.
+* Return: a / b.
 */
 int op_div(int a, int b)
 {
@@ -51,11 +45,10 @@ exit(100);
 return (a / b);
 }
 /**
-* op_mod - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
+* op_mod - finds modulus of two numbers
+* @a: first number.
+* @b: second number.
+* Return: a % b.
 */
 int op_mod(int a, int b)
 {
